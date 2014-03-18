f1d8adc73ca2d8ce51966bd74bb8decf0d929493
Harry Glatz <glatz@analog.de>
2014-03-18 09:14:12 +0100

    [TASK] Issue #1  Added Content- and PageController

---
+++ Classes/Controller/PageController.php
@@ -0,0 +1,66 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Claus Due <claus@namelesscoder.net>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Page Controller
+ *
+ * Controller which is used to render individual page template
+ * files - each template corresponding to one action.
+ *
+ * @package FluidpagesBootstrap
+ * @subpackage Controller
+ * @route off
+ */
+class Tx_Themebootstrap_Controller_PageController extends Tx_Fluidpages_Controller_AbstractPageController {
+
+	/**
+	 * @return string
+	 */
+	public function frontPageAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function renderAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function withSideBarAction() {
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function errorAction() {
+
+	}
+
+}
