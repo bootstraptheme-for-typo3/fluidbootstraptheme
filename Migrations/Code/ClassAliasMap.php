24dc40cf0f8dcd69246477b6f62c7989411cd7f3
Claus Due <claus@namelesscoder.net>
2014-03-27 16:31:53 +0100

    [TASK] Add ClassAliasMap

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+	'Tx_Fluidbootstraptheme_Controller_PageController' => 'FluidBT\Fluidbootstraptheme\Controller\PageController',
+	'Tx_Fluidbootstraptheme_Controller_ContentController' => 'FluidBT\Fluidbootstraptheme\Controller\ContentController',
+);
