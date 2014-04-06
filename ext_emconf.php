2f6e113db699c5f2bf2f4e0e0f8ac4b62baa4358
Benjamin Rau <rau@codearts.at>
2014-04-06 23:45:09 +0200

    [TASK] Fix dependencies and indentations in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,10 +15,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A collection of Twitter Bootstrap oriented Fluid Content Elements and Pages written for EXT:fluidcontent and EXT:fluidpages using ViewHelpers from EXT:vhs',
 	'category' => 'misc',
 	'author' => 'Claus Due, Harry Glatz, Boris Schauer ',
-    'author_email' => 'claus@namelesscoder.net,glatz@analog.de, me@bschauer.de',
-    'author_company' => 'NamelessCoder,analog multimedia, bschauer',
+    'author_email' => 'claus@namelesscoder.net, glatz@analog.de, me@bschauer.de',
+    'author_company' => 'NamelessCoder, analog multimedia, bschauer',
 	'shy' => '',
-	'dependencies' => 'cms,fluidcontent,fluidpages,vhs',
+	'dependencies' => 'cms,flux,fluidcontent,fluidpages,vhs',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -34,9 +34,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'typo3' => '6.1.00-6.2.99',
 			'cms' => '',
-            'flux' => '7.1.0',
+			'flux' => '7.1.0',
 			'fluidcontent' => '',
-            'fluidpages' => '',
+			'fluidpages' => '',
 			'vhs' => '',
 		),
 		'conflicts' => array(
@@ -48,5 +48,3 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'_md5_values_when_last_written' => 'a:20:{s:20:"class.ext_update.php";s:4:"eed2";s:12:"ext_icon.gif";s:4:"68b4";s:14:"ext_tables.php";s:4:"a37e";s:9:"README.md";s:4:"6a33";s:40:"Classes/Controller/ContentController.php";s:4:"e80f";s:34:"Configuration/TypoScript/setup.txt";s:4:"ffa7";s:40:"Resources/Private/Language/locallang.xml";s:4:"078d";s:50:"Resources/Private/Templates/Content/Accordion.html";s:4:"b4ca";s:46:"Resources/Private/Templates/Content/Alert.html";s:4:"b126";s:52:"Resources/Private/Templates/Content/ButtonGroup.html";s:4:"f41a";s:51:"Resources/Private/Templates/Content/ButtonLink.html";s:4:"e45c";s:49:"Resources/Private/Templates/Content/Carousel.html";s:4:"5b7b";s:49:"Resources/Private/Templates/Content/FluidRow.html";s:4:"078e";s:49:"Resources/Private/Templates/Content/HeroUnit.html";s:4:"1f8e";s:55:"Resources/Private/Templates/Content/NavigationList.html";s:4:"8f0b";s:51:"Resources/Private/Templates/Content/PageHeader.html";s:4:"7dbe";s:45:"Resources/Private/Templates/Content/Tabs.html";s:4:"d699";s:51:"Resources/Private/Templates/Content/Thumbnails.html";s:4:"ceb1";s:45:"Resources/Private/Templates/Content/Well.html";s:4:"2c4a";s:39:"Resources/Public/Icons/icon-twitter.png";s:4:"9450";}',
 );
-
-?>
