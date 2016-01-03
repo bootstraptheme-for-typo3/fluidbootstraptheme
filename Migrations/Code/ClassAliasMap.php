09cf5872b7309e4ebfc0e486d1332a47131bee76
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2016-01-03 15:02:16 +0100

    [TASK] Remove obsolete migration mapping

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'Tx_Fluidbootstraptheme_Controller_PageController' => 'FluidBT\Fluidbootstraptheme\Controller\PageController',
-	'Tx_Fluidbootstraptheme_Controller_ContentController' => 'FluidBT\Fluidbootstraptheme\Controller\ContentController',
-);
